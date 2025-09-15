bd73362e89dd9b9a9bdb64cf33f2b5fcffcdc5dc
crisp-coder <crisp-coder@proton.me>
2025-09-15 08:47:06 -0500

    Added a dummy test for auth

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,7 +1,21 @@
 package auth
 
-import "testing"
+import (
+	"net/http"
+	"strings"
+	"testing"
+)
 
 func TestGetAPIKey(t *testing.T) {
+	reader := strings.NewReader("this is hte request body content")
+	r, err := http.NewRequest("GET", "https://google.com", reader)
+	r.Header.Add("Authorization", "ApiKey 1234")
+	if err != nil {
+		t.Fatalf("error making request")
+	}
+	_, err = GetAPIKey(r.Header)
+	if err != nil {
+		t.Fatalf("error getting api key making request")
+	}
 
 }
